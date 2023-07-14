f20ad675d361970dacdee7b9413271dc831033f9
Jamie Long <jtlong3rd@gmail.com>
2023-07-14 01:03:24 -0400

    Test Vercel's Edge Network CDN

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,1 +1,19 @@
+import { HeadersFunction, json, LoaderArgs } from "@vercel/remix";
+
+export async function loader(_: LoaderArgs) {
+  await new Promise((resolve) => {
+    setTimeout(resolve, 1000);
+  });
+
+  return json(null, {
+    headers: {
+      "Cache-Control": "s-maxage=60, stale-while-revalidate=30",
+    },
+  });
+}
+
+export const headers: HeadersFunction = ({ loaderHeaders }) => {
+  return loaderHeaders;
+};
+
 export { default } from "~/app/components/App";
