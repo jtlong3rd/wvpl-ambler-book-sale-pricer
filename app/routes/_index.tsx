19d5e64086b3972d6c92a9fcb4c258c3f2542d16
Jamie Long <jtlong3rd@gmail.com>
2023-07-14 01:14:01 -0400

    Remove artifical slowdown

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,20 +1,8 @@
-import { HeadersFunction, json, LoaderArgs } from "@vercel/remix";
+import { HeadersFunction } from "@vercel/remix";
 
 export const config = {
   runtime: "edge",
 };
-
-export async function loader(_: LoaderArgs) {
-  await new Promise((resolve) => {
-    setTimeout(resolve, 1000);
-  });
-
-  return json(null, {
-    headers: {
-      "Cache-Control": "s-maxage=60, stale-while-revalidate=30",
-    },
-  });
-}
 
 export const headers: HeadersFunction = ({ loaderHeaders }) => {
   return loaderHeaders;
